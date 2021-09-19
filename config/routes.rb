53e6fb8bda33b1ec02cde7612439740f4123cf48
Precious chiedu <udegbue69@gmail.com>
2021-09-19 15:08:32 +0100

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'static#index'
   namespace :api do
     namespace :v1 do
       resources :users #only: %i[create index]
       resources :products
+      resources :reviews
       resources :orders
       post 'login', to: 'users#login'
       get 'auto_login', to: 'users#auto_login'
