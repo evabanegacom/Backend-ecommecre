b832d88c8b89ef21b2e45f5ba1ff1a3af4ee95c6
Precious chiedu <udegbue69@gmail.com>
2022-01-12 08:10:00 +0100

    added tests for user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :products, dependent: :destroy
   has_many :orders, dependent: :destroy
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   def as_json(options = {})
     super(options.merge({ except: %i[password_digest created_at updated_at] }))
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :feature do
+  context 'Validation tests' do
+    it 'ensures name presence' do
+      user = User.new(name: '', email: 'precious@yaho.com', password: 'precious', password_confirmation: 'precious').save
+      expect(user).to eql(false)
+    end
+
+    it 'ensures email presence' do
+      user = User.new(name: 'Precious', email: '', password: 'precious', password_confirmation: 'precious').save
+      expect(user).to eql(false)
+    end
+
+    it 'ensures password presence' do
+      user = User.new(name: 'Precious', email: 'precious@yaho.com', password: '', password_confirmation: 'precious').save
+      expect(user).to eql(false)
+    end
+
+    it 'ensures password_confirmation' do
+      user = User.new(name: 'Precious', email: 'precious@yaho.com', password: 'precious', password_confirmation: '').save
+      expect(user).to eql(false)
+    end
+
+    it 'ensures password and passsword_confirmation should match' do
+      user = User.new(name: 'Precious', email: 'precious@yaho.com', password: 'precious', password_confirmation: 'precious5').save
+      expect(user).to eql(false)
+    end
+
+    it 'ensures has a default phone and admin value' do
+      user = User.new(name: 'Precious', email: 'precious@yaho.com', password: 'precious', password_confirmation: 'precious').save
+      expect(user).to eql(true)
+    end
+  end
 end
